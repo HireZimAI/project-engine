fd9577ee04bd4258f635e0ec84e56ae4836eac0c
root <root@srv1462817.hstgr.cloud>
2026-04-14 21:25:48 +0000

    Pass API key through next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   allowedDevOrigins: ['187.124.88.125'],
+  env: {
+    'ANTHROPIC_API_KEY': process.env.ANTHROPIC_API_KEY,
+  },
 };
 
 export default nextConfig;
